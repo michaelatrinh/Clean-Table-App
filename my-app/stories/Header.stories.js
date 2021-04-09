ce1cc082cef7d3eeeb0f6bf8120315689f2e4cf3
JessicaW0 <jwu326@my.bcit.ca>
2021-04-09 02:52:33 -0700

    Empty Box comp done

--- my-app/stories/Header.stories.js
+++ my-app/stories/Header.stories.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import HeaderUI from '../comps/Header';
 import LoadingBarUI from '../comps/LoadingBar';
+import EmptyBoxUI from '../comps/EmptyBox';
 export default {
     title:"Jessica's Components",
     component:<HeaderUI/>,
-    component:<LoadingBarUI/>
+    component:<LoadingBarUI/>,
+    component:<EmptyBoxUI/>
 }
 
 export const MyHeaderUI = () => <HeaderUI/>
 export const MyLoadingBarUI = () => <LoadingBarUI/>
+export const MyEmptyBoxUI = () => <EmptyBoxUI/>
 
