01e4bf49047cffb086dba343380ecb271c730f20
JessicaW0 <jwu326@my.bcit.ca>
2021-04-09 04:19:26 -0700

    Home Food Timer comp done

--- my-app/stories/Header.stories.js
+++ my-app/stories/Header.stories.js
@@ -2,14 +2,17 @@ import React from 'react';
 import HeaderUI from '../comps/Header';
 import LoadingBarUI from '../comps/LoadingBar';
 import EmptyBoxUI from '../comps/EmptyBox';
+import HomeFoodTimerUI from '../comps/HomeFoodTimer';
+
 export default {
     title:"Jessica's Components",
     component:<HeaderUI/>,
     component:<LoadingBarUI/>,
-    component:<EmptyBoxUI/>
+    component:<EmptyBoxUI/>,
+    component:<HomeFoodTimerUI/>
 }
 
 export const MyHeaderUI = () => <HeaderUI/>
 export const MyLoadingBarUI = () => <LoadingBarUI/>
 export const MyEmptyBoxUI = () => <EmptyBoxUI/>
-
+export const MyHomeFoodTimerUI = () => <HomeFoodTimerUI/>
