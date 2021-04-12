6c04357804b2e18fc420ce251e939ebd134f1bb2
Michael <mtrinh8@my.bcit.ca>
2021-04-11 17:21:55 -0700

    made recipe instructions comp

---
+++ my-app/stories/Recipe-Instructions.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Recipe from '../comps/Recipe-Instructions';
+
+export default{
+    title:"Michael's Components/Recipe Instructions",
+    component: <Recipe/>
+}
+
+export const RecipeInstruction = () => <Recipe/>
