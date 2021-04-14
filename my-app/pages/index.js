f0b7180b5d769a8c84bbc4a92d3b91bb0853c777
michaelBCIT <72416671+michaelBCIT@users.noreply.github.com>
2021-04-14 15:53:47 -0700

    Add files via upload

---
+++ my-app/pages/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import styled from 'styled-components';
+import FoodButton from '../comps/FoodButtonUI';
+import ProgressBar from '../comps/ProgressBar';
+import Heading from '../comps/Header';
