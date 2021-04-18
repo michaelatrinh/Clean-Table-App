650f73896c9d0c4d8c9bf7a461d3a5938537997b
Michael <mtrinh8@my.bcit.ca>
2021-04-18 00:27:27 -0700

    pantry category page routing work

--- my-app/pages/pantry-pick-category.js
+++ my-app/pages/pantry-pick-category.js
@@ -14,26 +14,58 @@ export default function PantryCategory({
     description = "Choose a category to add more items to your pantry list!",
     routeTo = "/pantry-add-protein"
 }){
-    const [clickState, setClickState] = useState(false);
-    var ShadowToggle = "0px 4px 5px #494948";
-    if(clickState){
-        ShadowToggle = "inset 0 0 10px 5px #5C5C5C";
+    // === BOX SHADOW STATE CHANGE SETUP ========================
+    const [clickStateP, setClickStateP] = useState(false);
+    const [clickStateC, setClickStateC] = useState(false);
+    const [clickStateV, setClickStateV] = useState(false);
+    var ShadowToggleP = "0px 4px 5px #494948";
+    var ShadowToggleC = "0px 4px 5px #494948";
+    var ShadowToggleV = "0px 4px 5px #494948";
+
+    if(clickStateP){
+        ShadowToggleP = "inset 0 0 10px 5px #5C5C5C";
+    } else if(clickStateC){
+        ShadowToggleC = "inset 0 0 10px 5px #5C5C5C";
+    } else if(clickStateV){
+        ShadowToggleV = "inset 0 0 10px 5px #5C5C5C";
     }
 
-    const HandleClick = () => {
-        setClickState(!clickState);
-        setTimeout(NextPage, 150);
+    // === BOX SHADOW STATE CHANGE AND ROUTING ==================
+    const HandleClickP = () => {
+        setClickStateP(!clickStateP);
+        setTimeout(NextPageP, 150);
+    }    
+    const HandleClickC = () => {
+        setClickStateC(!clickStateC);
+        setTimeout(NextPageC, 150);
+    }    
+    const HandleClickV = () => {
+        setClickStateV(!clickStateV);
+        setTimeout(NextPageV, 150);
     }
 
     const router = useRouter();
 
-    const NextPage = () => {
-        var routeTo = "/pantry-add-protein";
+    const NextPageP = () => {
+        var routeToProteins = "/pantry-add-protein";
         // var routeToCarbs = "/pantry-add-carbohydrate";
         // var routeToVeges = "/pantry-add-vegetable";
-        router.push(routeTo);
+        router.push(routeToProteins);
+    }    
+    const NextPageC = () => {
+        // var routeTo = "/pantry-add-protein";
+        var routeToCarbs = "/pantry-add-carbohydrate";
+        // var routeToVeges = "/pantry-add-vegetable";
+        router.push(routeToCarbs);
+    }    
+    const NextPageV = () => {
+        // var routeTo = "/pantry-add-protein";
+        // var routeToCarbs = "/pantry-add-carbohydrate";
+        var routeToVeges = "/pantry-add-vegetable";
+        router.push(routeToVeges);
     }
 
+    // === PAGE DISPLAY =========================================
     return <PageContainer>
         <HeaderContainer>
             <Heading></Heading>
@@ -45,15 +77,17 @@ export default function PantryCategory({
         </TextContainer>
 
         <CategoryContainer>
-            <CategoryButton onClick={HandleClick} shadow={ShadowToggle}>
+            <CategoryButtonP onClick={HandleClickP} shadowP={ShadowToggleP}>
                 <h2>PROTEINS</h2>
-            </CategoryButton>
-            <CategoryButton onClick={HandleClick} shadow={ShadowToggle}>
+            </CategoryButtonP>
+
+            <CategoryButtonC onClick={HandleClickC} shadowC={ShadowToggleC}>
                 <h2>CARBOHYDRATES</h2>
-            </CategoryButton>
-            <CategoryButton onClick={HandleClick} shadow={ShadowToggle}>
+            </CategoryButtonC>
+
+            <CategoryButtonV onClick={HandleClickV} shadowV={ShadowToggleV}>
                 <h2>VEGETABLES</h2>
-            </CategoryButton>
+            </CategoryButtonV>
         </CategoryContainer>
 
         <NavBarContainer>
@@ -114,12 +148,42 @@ const CategoryContainer = styled.div`
     margin-top: -5rem;
 `;
 
-const CategoryButton = styled.div`
+const CategoryButtonP = styled.div`
     display: flex;
-    justify-content: space-evenly;
+    justify-content: center;
     align-items: center;
     background-color: #59A091;
-    box-shadow: ${props=>props.shadow};
+    box-shadow: ${props=>props.shadowP};
+    width: 237px;
+    height: 74px;
+    &>h2 {
+        font-family: "Manrope", sans-serif;
+        font-size: 24px;
+        color: #FFFFFF;
+    }
+`;
+
+const CategoryButtonC = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #59A091;
+    box-shadow: ${props=>props.shadowC};
+    width: 237px;
+    height: 74px;
+    &>h2 {
+        font-family: "Manrope", sans-serif;
+        font-size: 24px;
+        color: #FFFFFF;
+    }
+`;
+
+const CategoryButtonV = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #59A091;
+    box-shadow: ${props=>props.shadowV};
     width: 237px;
     height: 74px;
     &>h2 {
