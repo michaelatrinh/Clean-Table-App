9c5e05a83322848ca774a05619a2998b0e71c3b9
Michael <mtrinh8@my.bcit.ca>
2021-04-25 21:02:31 -0700

    added a missing import React on a page

--- my-app/pages/page3-5/index.js
+++ my-app/pages/page3-5/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Head from 'next/head';
 import styled from 'styled-components';
-import React, {useState} from 'react';
+
 
 import HeaderUI from '../../comps/Header';
 import FoodButtonUI from '../../comps/FoodButtonUI';
