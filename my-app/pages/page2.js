a62dd06fd91cfa2ad99e82b54ae8e81392e28cb3
Michael <mtrinh8@my.bcit.ca>
2021-05-11 12:29:36 -0700

    grabbed arielles code for page2.js and enterbutton.js

--- my-app/pages/page2.js
+++ my-app/pages/page2.js
@@ -3,7 +3,7 @@ import animationStyle from '../styles/animation.module.css';
 import HeaderUI from '../comps/Header';
 import StartTutorialButtonUI from '../comps/StartTutorialButton';
 import styled from 'styled-components';
-
+ 
 export default function Page2({
     title ="HOW TO GET STARTED",
     description ="People want to know the best method when it comes to managing perishability.",
@@ -21,6 +21,10 @@ export default function Page2({
                 <PageTextTwo>{descriptiontwo}</PageTextTwo>
             </TextContainer>
     
+    <StartButton>
+    <StartTutorialButtonUI routeTo = "/add-protein"></StartTutorialButtonUI>
+    </StartButton>
+ 
     <Animation> <div className={animationStyle.main}>
             <div className={animationStyle.shadowwrapper}>
                 <div className={animationStyle.shadow}></div>
@@ -38,33 +42,30 @@ export default function Page2({
                 </div>
                 </div>
      </Animation>
-    
-    <StartButton>
-    <StartTutorialButtonUI routeTo = "/add-protein"></StartTutorialButtonUI>
-    </StartButton>
-        </PageContainer>
-        
+</PageContainer>
    }
+
 const PageContainer = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
-
+    align-items: center;
+ 
     background-color: #E7F2F0;
     width: 100vw;
     max-width: 100vw;
     height: 100vh;
     max-height: 100vh;
 `;
-
+ 
 const HeaderContainer =styled.div`
 display: flex;
 justify-content: center;
 align-items: center;
 flex-direction: column;
-
+ 
  `;
-
+ 
  const TextContainer = styled.div`
     display: flex;
     flex-direction: column;
@@ -72,7 +73,7 @@ flex-direction: column;
     justify-content: center;
     flex-grow: 1;
 `;
-
+ 
 const PageTitle = styled.h1`
     font-family: "Manrope", sans-serif;
     font-size: 24px;
@@ -80,7 +81,7 @@ const PageTitle = styled.h1`
     position:relative;
     top:-70px;
 `;
-
+ 
 const PageText = styled.h2`
 font-family: "Quicksand", sans-serif;
     font-size: 20px;
@@ -91,7 +92,7 @@ font-family: "Quicksand", sans-serif;
     position:relative;
     top:-60px;
 `;
-
+ 
 const PageTextTwo = styled.h2`
 font-family: "Quicksand", sans-serif;
     font-size: 20px;
@@ -102,22 +103,145 @@ font-family: "Quicksand", sans-serif;
     position:relative;
     top:-60px;
 `;
-
+ 
 const Animation = styled.div`
-display: flex: display;
+display: flex;
 position:relative;
 top:-10px;
-left:70px;
-
+ 
+ 
  `;
-
-
+ 
+ 
  const StartButton =styled.div`
  display: flex;
  flex-direction: column;
  align-items: center;
  justify-content: center;
  position:relative;
-    bottom:35px;
+ bottom:200px;
  `;
+
+// import React, {useState} from 'react';
+// import animationStyle from '../styles/animation.module.css';
+// import HeaderUI from '../comps/Header';
+// import StartTutorialButtonUI from '../comps/StartTutorialButton';
+// import styled from 'styled-components';
+
+// export default function Page2({
+//     title ="HOW TO GET STARTED",
+//     description ="People want to know the best method when it comes to managing perishability.",
+//     descriptiontwo=" With this app you can now know how long food can actually last beyond best before date."
+// }){
+ 
+//     return <PageContainer>
+//     <HeaderContainer>
+//         <HeaderUI></HeaderUI>
+//     </HeaderContainer>
+    
+//     <TextContainer>
+//                 <PageTitle>{title}</PageTitle>
+//                 <PageText>{description}</PageText>
+//                 <PageTextTwo>{descriptiontwo}</PageTextTwo>
+//             </TextContainer>
+    
+//     <Animation> <div className={animationStyle.main}>
+//             <div className={animationStyle.shadowwrapper}>
+//                 <div className={animationStyle.shadow}></div>
+//             </div>
+//             <div className={animationStyle.food}>
+//                 <div className={animationStyle.body}></div>
+//                 <div className={animationStyle.eyeLeft}></div>
+//                 <div className={animationStyle.eyeRight}></div>
+//                 <div className={animationStyle.blushLeft}></div>
+//                 <div className={animationStyle.blushRight}></div>
+//                 <div className={animationStyle.mouth}></div>
+//             </div>
+//             <div className={animationStyle.hiwrapper}>
+//                 <div className={animationStyle.hi}></div>
+//                 </div>
+//                 </div>
+//      </Animation>
+    
+//     <StartButton>
+//     <StartTutorialButtonUI routeTo = "/add-protein"></StartTutorialButtonUI>
+//     </StartButton>
+//         </PageContainer>
+        
+//    }
+// const PageContainer = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: center;
+
+//     background-color: #E7F2F0;
+//     width: 100vw;
+//     max-width: 100vw;
+//     height: 100vh;
+//     max-height: 100vh;
+// `;
+
+// const HeaderContainer =styled.div`
+// display: flex;
+// justify-content: center;
+// align-items: center;
+// flex-direction: column;
+
+//  `;
+
+//  const TextContainer = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     justify-content: center;
+//     flex-grow: 1;
+// `;
+
+// const PageTitle = styled.h1`
+//     font-family: "Manrope", sans-serif;
+//     font-size: 24px;
+//     font-weight: 400;
+//     position:relative;
+//     top:-70px;
+// `;
+
+// const PageText = styled.h2`
+// font-family: "Quicksand", sans-serif;
+//     font-size: 20px;
+//     font-weight: 300;
+//     margin-right: 50px;
+//     margin-left: 50px;
+//     text-align:center;
+//     position:relative;
+//     top:-60px;
+// `;
+
+// const PageTextTwo = styled.h2`
+// font-family: "Quicksand", sans-serif;
+//     font-size: 20px;
+//     font-weight: 300;
+//     margin-right: 50px;
+//     margin-left: 50px;
+//     text-align:center;
+//     position:relative;
+//     top:-60px;
+// `;
+
+// const Animation = styled.div`
+// display: flex: display;
+// position:relative;
+// top:-10px;
+// left:70px;
+
+//  `;
+
+
+//  const StartButton =styled.div`
+//  display: flex;
+//  flex-direction: column;
+//  align-items: center;
+//  justify-content: center;
+//  position:relative;
+//     bottom:35px;
+//  `;
 
