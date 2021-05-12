d641d10644d2e630665516adb93bd6a1c3414e15
JessicaW0 <72417252+JessicaW0@users.noreply.github.com>
2021-05-12 16:08:53 -0700

    trying to merge

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,47 +1,17 @@
 import React from 'react';
 import Head from 'next/head'; 
 import styled from 'styled-components';
-import Header from '../comps/Header/index.js';
-import Menu from '../comps/NavBar/index.js';
-import Placeholder from '../comps/icon-placeholder/Placeholder.js';
-import FunFactWindow from '../comps/FunFactWindow/index.js';
-import RecipeButton from '../comps/Recipe-Button/index.js';
+import Button from '../comps/EnterButton';
+import Login from '../comps/Login';
+import Footer from '../comps/WelcomePageFooter';
 
-const RecipeContainer = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
 
-background-color: #E7F2F0;
-width: 100vw;
-max-width: 100vw;
-height: 100vh;
-max-height: 100vh;
-`;
-
-const HeaderContainer = styled.div`
-    display: flex;
-    flex-grow: 0;
-`;
-
-const TopSection = styled.div`
+const FirstPageContainer = styled.div`    
     display: flex;
     flex-direction: column;
-<<<<<<< HEAD
-    justify-content: center;
     align-items: center;
-    flex-grow: 1;
-=======
-    align-items: center;
-`;
-
-const TopSection = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    flex-grow: 1;
+    height: 100vh;
+    width: 100%;
 
     background: linear-gradient(-45deg, #59a091, #d4f6f4, #ffffff);
     background-size: 400% 400%;
@@ -64,134 +34,67 @@ const TopSection = styled.div`
         font-weight: bold;
         color: #302B40;
     }
-`;
->>>>>>> 0fe0225c5f1cdc481a2d2606f58ec451d552b164
 
-    h3{
-        font-size: 22px;
-    }
-    img{
-        width: 281px;
-        height:165px;
-
-        border: 4px solid #59A091;
-        // margin-top: 80px;
+    p{
+        color: #302B40;
     }
 `;
+ 
+const Img = styled.img`
+    margin-top: 350px;
+    width: 200px;
 
-const MiddleContainer = styled.div`
     display: flex;
-    justify-content: space-evenly;
+    justify-content: center;
     align-items: center;
-    flex-direction: column;
-    flex-grow: 3;
-`;
 
-const Mid1 = styled.div`
-    // margin-top: 160px;
-`;
-
-const Mid2 = styled.div`
-    // margin-top: -70px;
-`;
-
-const Mid3 = styled.div`
-    // margin-top: -30px;
-`;
-
-const EndSection = styled.div`
-    // position: absolute;
-    // margin-top: 800px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    align-items: center;
-    flex-grow: 2;
 `;
 
 const ButtonContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-grow: 1;
+    margin-top: 10px;
+    position: relative;
 `;
 
-const MenuContainer = styled.div`
-    // margin-top: 950px;
-    // height: 900px;
-    // position: absolute;
-    // overflow: hidden;
+const Circle = styled.div`
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-grow: 0;
+    height: 160px;
+    width: 375px;
+
+    border-top-right-radius: 50%;
+    border-top-left-radius: 50%;
+
+    position: relative;
+    margin-top: 400px;
+    overflow: hidden;
+
+    color: #302B40;
+    text-align: center;
+
+    p{
+        margin-top: 60px;
+    }
 `;
-<<<<<<< HEAD
 
+export default function FirstPage({
+    logoimage = "/logo.png",
+    text = "enter"
 
-export default function RecipePage({
-    recipeimage = "/beefbrocrice.jpg",
-    recipetitle = "Beef Rice Casserole",
-
-    image1 = "/shaved_beef.png",
-    image2 = "/rice_bowl.png",
-    image3 = "/broccoli.png",
-
-    image4 ="/saltnpepper.png",
-    image5 ="/cheese.png",
-    image6 ="/parsley.png",
-
-    label1 = "BEEF",
-    label2 = "RICE",
-    label3 = "BROCCOLI",
-    label4 = "SALT N PEPPER",
-    label5 = "CHEESE",
-    label6 = "PARSLEY",
-
-    subtitle = "Main Ingredients",
-    htext = "DID YOU KNOW?",
-    ptext = "Preserve produce. Produce doesn’t have to be tossed just because it’s reaching the end of its peak. Soft fruit can be used in smoothies; wilting vegetables can be used in soups, etc."
-    
 }) {
 
+/*const HomepageUI = ({
+    logoimage = "/logo.png",
 
-      return <RecipeContainer>
-          <HeaderContainer>
-            <Header></Header>
-          </HeaderContainer>
-
-        <TopSection>
-            <h3>{recipetitle}</h3>
-            <img src={recipeimage}></img>
-        </TopSection>
-
-        <MiddleContainer>
-            <Mid1>
-            <Placeholder height="150px" image1={image1} image2={image2} image3={image3} label1={label1} label2={label2} label3={label3} subtitle={"Main Ingredients"} widthA="60px" widthB="65px" widthC="50px"></Placeholder>
-            </Mid1>
-            <Mid2>
-                <Placeholder height="150px" image1={image4} image2={image5} image3={image6} subtitle={"Optionals"} label1={label4} label2={label5} label3={label6}></Placeholder>
-            </Mid2>
-            {/* <Mid3>
-                <RecipeButton Link href="https://tasty.co/recipe/one-pan-garlic-parmesan-chicken-and-vegetable-bake"></RecipeButton>
-            </Mid3> */}
-        </MiddleContainer>
+}) => {*/
+      return <FirstPageContainer>
+        <Img src={logoimage}></Img>
+        <p className="headertext">W E L C O M E </p>
+        <ButtonContainer>
+            <Button text={text}></Button>
+        </ButtonContainer>
         
-            <ButtonContainer>
-                <RecipeButton onClick={()=>window.open("https://www.dinneratthezoo.com/beef-and-broccoli-stir-fry/")}></RecipeButton>
-            </ButtonContainer>
-
-        <EndSection>                
-
-
-            <FunFactWindow ptext={"Preserve produce. Produce doesn’t have to be tossed just because it’s reaching the end of its peak. Soft fruit can be used in smoothies; wilting vegetables can be used in soups, etc."}> </FunFactWindow>
-        </EndSection>
-
-        <MenuContainer>
-            <Menu></Menu>
-        </MenuContainer>
-    </RecipeContainer>  
+        <Circle>
+            <p>created by: <br></br>
+Jessica, Arielle, Michael, & Alicia</p>
+        </Circle>
+    </FirstPageContainer>  
 }
-=======
->>>>>>> 0fe0225c5f1cdc481a2d2606f58ec451d552b164
