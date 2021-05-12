2a428f5086f1d2f3e9a42b2e151a564cca0d5a8b
JessicaW0 <72417252+JessicaW0@users.noreply.github.com>
2021-05-12 11:46:50 -0700

    commit

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,41 +1,37 @@
 import React from 'react';
 import Head from 'next/head'; 
 import styled from 'styled-components';
-import HeaderUI from '../comps/Header';
-import LoadingBar from '../../comps/LoadingBar/index.js';
+import Button from '../comps/EnterButton';
+import Login from '../comps/Login';
+import Footer from '../comps/WelcomePageFooter';
 
-export default function LoadingUI({
 
-
-}) {
-
-    // window.onload
-
-/*const HomepageUI = ({
-    logoimage = "/logo.png",
-
-}) => {*/
-      return <LoadingContainer>
-          <Header></Header>
-          
-          <LMidSection>
-              <LoadingBar></LoadingBar>
-          </LMidSection>
-          
-    </LoadingContainer>  
-}
-
-const LoadingContainer = styled.div`
+const FirstPageContainer = styled.div`
     width: 375px;
     height: 812px;
-    background-color: #E7F2F0;
     margin: 100px;
     
     display: flex;
     flex-direction: column;
     align-items: center;
 
-    box-shadow: 5px 1px 30px 5px rgba(0, 0, 0, 0.4);
+    box-shadow: 5px 1px 30px 5px rgba(0, 0, 0, 0.5);
+
+    background: linear-gradient(-45deg, #59a091, #d4f6f4, #ffffff);
+    background-size: 400% 400%;
+    animation: gradient 15s ease infinite;
+    
+    @keyframes gradient {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
+    }
 
     .headertext{
         font-size: 36px;
@@ -44,10 +40,62 @@ const LoadingContainer = styled.div`
     }
 `;
  
-const LMidSection = styled.div`
+const Img = styled.img`
+    margin-top: 200px;
+    width: 200px;
+
     display: flex;
     justify-content: center;
+    align-items: center;
 
-    margin-top: 300px;
 `;
 
+const ButtonContainer = styled.div`
+    margin-top: 10px;
+    position: relative;
+`;
+
+const Circle = styled.div`
+    background-color: #59A091;
+    height: 160px;
+    width: 375px;
+
+    border-top-right-radius: 50%;
+    border-top-left-radius: 50%;
+
+    position: relative;
+    margin-top: 80px;
+    overflow: hidden;
+
+    color: white;
+    text-align: center;
+
+    p{
+        margin-top: 60px;
+    }
+`;
+
+export default function FirstPage({
+    logoimage = "/logo.png",
+    text = "enter"
+
+}) {
+
+/*const HomepageUI = ({
+    logoimage = "/logo.png",
+
+}) => {*/
+      return <FirstPageContainer>
+        <Img src={logoimage}></Img>
+        <p className="headertext">W E L C O M E </p>
+        <Login></Login>
+        <ButtonContainer>
+            <Button text={text} height="34px"></Button>
+        </ButtonContainer>
+        
+        <Circle>
+            <p>created by: <br></br>
+Jessica, Arielle, Michael, & Alicia</p>
+        </Circle>
+    </FirstPageContainer>  
+}
