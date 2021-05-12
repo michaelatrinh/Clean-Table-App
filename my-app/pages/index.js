eb3576271b431d2d7095cbfacf595507ea7cd5ef
JessicaW0 <72417252+JessicaW0@users.noreply.github.com>
2021-05-12 16:53:06 -0700

    comit page 1

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,100 +1,155 @@
 import React from 'react';
 import Head from 'next/head'; 
 import styled from 'styled-components';
-import Button from '../comps/EnterButton';
-import Login from '../comps/Login';
-import Footer from '../comps/WelcomePageFooter';
+import Header from '../comps/Header/index.js';
+import Menu from '../comps/NavBar/index.js';
+import Placeholder from '../comps/icon-placeholder/Placeholder.js';
+import FunFactWindow from '../comps/FunFactWindow/index.js';
+import RecipeButton from '../comps/Recipe-Button/index.js';
+
+export default function RecipePage({
+    recipeimage = "/onionchicbake.png",
+    recipetitle = "Baked Chicken And Potatoes",
+
+    image1 = "/chicken_drumstick.png",
+    image2 = "/potatoes.png",
+    image3 = "/onion.png",
+
+    image4 ="/saltnpepper.png",
+    image5 ="/cheese.png",
+    image6 ="/parsley.png",
+
+    label1 = "CHICKEN",
+    label2 = "PASTA",
+    label3 = "CARROT",
+    label4 = "SALT N PEPPER",
+    label5 = "CHEESE",
+    label6 = "PARSLEY",
+
+    subtitle = "Main Ingredients",
+
+    
+}) {
 
 
-const FirstPageContainer = styled.div`    
+      return <RecipeContainer>
+          <HeaderContainer>
+            <Header></Header>
+          </HeaderContainer>
+
+        <TopSection>
+            <h3>{recipetitle}</h3>
+            <img src={recipeimage}></img>
+        </TopSection>
+
+        <MiddleContainer>
+            <Mid1>
+            <Placeholder height="150px" image1={image1} image2={image2} image3={image3} label1={label1} label2={label2} label3={label3} subtitle={"Main Ingredients"} widthA="60px" widthB="65px" widthC="50px"></Placeholder>
+            </Mid1>
+            <Mid2>
+                <Placeholder height="150px" image1={image4} image2={image5} image3={image6} subtitle={"Optionals"} label1={label4} label2={label5} label3={label6}></Placeholder>
+            </Mid2>
+      {/* <Mid3>
+          <RecipeButton Link href="https://tasty.co/recipe/one-pan-garlic-parmesan-chicken-and-vegetable-bake"></RecipeButton>
+      </Mid3> */}
+  </MiddleContainer>
+  
+      <ButtonContainer>
+          <RecipeButton onClick={()=>window.open("https://tasty.co/recipe/one-pan-garlic-parmesan-chicken-and-vegetable-bake")}></RecipeButton>
+      </ButtonContainer>
+
+  <EndSection>                
+      <FunFactWindow></FunFactWindow>
+  </EndSection>
+
+  <MenuContainer>
+      <Menu></Menu>
+  </MenuContainer>
+</RecipeContainer>   
+}
+
+const RecipeContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+background-color: #E7F2F0;
+width: 100vw;
+max-width: 100vw;
+height: 100vh;
+max-height: 100vh;
+`;
+
+const HeaderContainer = styled.div`
+    display: flex;
+
+`;
+
+const TopSection = styled.div`
     display: flex;
     flex-direction: column;
+    justify-content: center;
     align-items: center;
-    height: 100vh;
-    width: 100%;
+    flex-grow: 1;
 
-    background: linear-gradient(-45deg, #59a091, #d4f6f4, #ffffff);
-    background-size: 400% 400%;
-    animation: gradient 15s ease infinite;
-    
-    @keyframes gradient {
-        0% {
-            background-position: 0% 50%;
-        }
-        50% {
-            background-position: 100% 50%;
-        }
-        100% {
-            background-position: 0% 50%;
-        }
+    h3{
+        font-size: 22px;
     }
+    img{
+        // position: absolute;
 
-    .headertext{
-        font-size: 36px;
-        font-weight: bold;
-        color: #302B40;
-    }
-
-    p{
-        color: #302B40;
+        border: 4px solid #59A091;
+        // margin-top: 80px;
     }
 `;
- 
-const Img = styled.img`
-    margin-top: 350px;
-    width: 200px;
+
+const MiddleContainer = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-direction: column;
+    flex-grow: 3;
+`;
+
+const Mid1 = styled.div`
+    // margin-top: 160px;
+`;
+
+const Mid2 = styled.div`
+    // margin-top: -70px;
+`;
+
+const Mid3 = styled.div`
+    // margin-top: -30px;
+`;
+
+const EndSection = styled.div`
+    // position: absolute;
+    // margin-top: 800px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-grow: 2;
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-grow: 1;
+`;
+
+const MenuContainer = styled.div`
+    // margin-top: 950px;
+    // height: 900px;
+    // position: absolute;
+    // overflow: hidden;
 
     display: flex;
     justify-content: center;
     align-items: center;
-
+    flex-grow: 0;
 `;
 
-const ButtonContainer = styled.div`
-    margin-top: 10px;
-    position: relative;
-`;
-
-const Circle = styled.div`
-
-    height: 160px;
-    width: 375px;
-
-    border-top-right-radius: 50%;
-    border-top-left-radius: 50%;
-
-    position: relative;
-    margin-top: 400px;
-    overflow: hidden;
-
-    color: #302B40;
-    text-align: center;
-
-    p{
-        margin-top: 60px;
-    }
-`;
-
-export default function FirstPage({
-    logoimage = "/logo.png",
-    text = "enter"
-
-}) {
-
-/*const HomepageUI = ({
-    logoimage = "/logo.png",
-
-}) => {*/
-      return <FirstPageContainer>
-        <Img src={logoimage}></Img>
-        <p className="headertext">W E L C O M E </p>
-        <ButtonContainer>
-            <Button text={text}></Button>
-        </ButtonContainer>
-        
-        <Circle>
-            <p>created by: <br></br>
-Jessica, Arielle, Michael, & Alicia</p>
-        </Circle>
-    </FirstPageContainer>  
-}
--- my-app/pages/page1.js
+++ my-app/pages/page1.js
@@ -4,15 +4,14 @@ import styled from 'styled-components';
 import Button from '../comps/EnterButton';
 import Login from '../comps/Login';
 import Footer from '../comps/WelcomePageFooter';
-
-
+ 
 const FirstPageContainer = styled.div`    
     display: flex;
     flex-direction: column;
     align-items: center;
     height: 100vh;
     width: 100%;
-
+ 
     background: linear-gradient(-45deg, #59a091, #d4f6f4, #ffffff);
     background-size: 400% 400%;
     animation: gradient 15s ease infinite;
@@ -28,13 +27,13 @@ const FirstPageContainer = styled.div`
             background-position: 0% 50%;
         }
     }
-
+ 
     .headertext{
         font-size: 36px;
         font-weight: bold;
         color: #302B40;
     }
-
+  
     p{
         color: #302B40;
     }
@@ -43,47 +42,47 @@ const FirstPageContainer = styled.div`
 const Img = styled.img`
     margin-top: 350px;
     width: 200px;
-
+ 
     display: flex;
     justify-content: center;
     align-items: center;
-
+ 
 `;
-
+ 
 const ButtonContainer = styled.div`
     margin-top: 10px;
     position: relative;
 `;
-
+ 
 const Circle = styled.div`
-
+ 
     height: 160px;
     width: 375px;
-
+ 
     border-top-right-radius: 50%;
     border-top-left-radius: 50%;
-
+ 
     position: relative;
     margin-top: 400px;
     overflow: hidden;
-
+ 
     color: #302B40;
     text-align: center;
-
+ 
     p{
         margin-top: 60px;
     }
 `;
-
+ 
 export default function FirstPage({
     logoimage = "/logo.png",
     text = "enter"
-
+ 
 }) {
-
+ 
 /*const HomepageUI = ({
     logoimage = "/logo.png",
-
+ 
 }) => {*/
       return <FirstPageContainer>
         <Img src={logoimage}></Img>
